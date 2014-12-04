209e5119df800c9c8cd079c0474e9e8cad723ca7
Robert Vazan <robvcode@gmail.com>
2014-12-04 16:12:50 +0100

    Generate getter/setter

--- AssemblyToProcess/Class1.cs
+++ AssemblyToProcess/Class1.cs
@@ -5,6 +5,10 @@ namespace AssemblyToProcess
 {
     public class Class1 : DependencyObject
     {
+        public readonly DependencyProperty AnotherProperty = DependencyProperty.Register("Another", typeof(int), typeof(Class1));
+        public int Another { get { return (int)GetValue(AnotherProperty); } set { SetValue(AnotherProperty, value); } }
+
         public string MyProp { get; set; }
+        public int IntProp { get; set; }
     }
 }
