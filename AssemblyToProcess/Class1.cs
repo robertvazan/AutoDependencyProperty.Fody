4170727fd4d432d4918b855b45ab80cae819fba6
Robert Vazan <robvcode@gmail.com>
2014-12-04 17:00:31 +0100

    Remove unused backing field

--- AssemblyToProcess/Class1.cs
+++ AssemblyToProcess/Class1.cs
@@ -4,14 +4,10 @@ using System.Windows;
 
 namespace AssemblyToProcess
 {
+    [DependencyProperty]
     public class Class1 : DependencyObject
     {
-        public readonly DependencyProperty AnotherProperty = DependencyProperty.Register("Another", typeof(int), typeof(Class1));
-        public int Another { get { return (int)GetValue(AnotherProperty); } set { SetValue(AnotherProperty, value); } }
-
-        [DependencyProperty]
         public string MyProp { get; set; }
-        [DependencyProperty]
         public int IntProp { get; set; }
     }
 }
