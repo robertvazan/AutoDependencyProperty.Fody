40613df297759fcb19351383c0bd8ba6f4c9f3c7
Robert Va?an <Robert Va?an@localhost>
2014-12-04 11:41:41 +0100

    Renamed the project

---
+++ FodyDependencyProperty/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("FodyDependencyProperty")]
+[assembly: AssemblyDescription("Fody plugin that converts automatic C# property into WPF DependencyProperty during build")]
+[assembly: AssemblyProduct("FodyDependencyProperty")]
+[assembly: AssemblyCompany("Robert Važan")]
+[assembly: AssemblyCopyright("Copyright © 2014 Robert Važan")]
+[assembly: AssemblyVersion("1.0.0")]
