c8a73b2d9afb9de775e985218682c640469f8c09
Robert Važan <robert.vazan@tutanota.com>
2021-03-15 08:45:37 +0000

    Updated copyright year

--- AutoDependencyProperty.Fody/AssemblyInfo.cs
+++ AutoDependencyProperty.Fody/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Reflection;
 [assembly: AssemblyDescription("Fody plugin that converts automatic C# property into WPF DependencyProperty during build")]
 [assembly: AssemblyProduct("AutoDependencyProperty.Fody")]
 [assembly: AssemblyCompany("Robert Važan")]
-[assembly: AssemblyCopyright("Copyright © 2014 Robert Važan")]
+[assembly: AssemblyCopyright("Copyright © 2014-2021 Robert Važan")]
 [assembly: AssemblyVersion("1.2.2.0")]
